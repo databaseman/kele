7a74cf9c765b224444d59a120336f13bfdda0ee0
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-02-06 18:59:07 +0000

    Refactor roadmap

--- kele.gemspec
+++ kele.gemspec
@@ -6,10 +6,9 @@ Gem::Specification.new do |s|
    s.description   = 'A client for the Bloc API'
    s.authors       = ['Minh Nguyen']
    s.email         = 'nguyen_ba_minh@yahoo.com'
-   s.files         = ['lib/kele.rb']
+   s.files         = `git ls-files "lib/*.rb"`.split($/)
    s.require_paths = ["lib"]
-   s.homepage      =
-     'http://rubygems.org/gems/kele'
+   s.homepage      = 'http://rubygems.org/gems/kele'
    s.license       = 'MIT'
    s.add_runtime_dependency 'httparty', '~> 0.13'
  end
--- lib/kele.rb
+++ lib/kele.rb
@@ -4,6 +4,7 @@ require 'roadmap'
 
 class Kele
   include HTTParty
+  include Roadmap
   base_uri 'https://www.bloc.io/api/v1'  
   
   def initialize(email, password)
@@ -30,15 +31,4 @@ class Kele
 		response = self.class.get( "/mentors/#{mentor_id}/student_availability", headers: {'authorization' => @auth_token} )
 		JSON.parse(response.body)
 	end
-
-	def get_roadmap(roadmap_id) #31
-		response = self.class.get( "/roadmaps/#{roadmap_id}", headers: {'authorization' => @auth_token} )
-		JSON.parse(response.body)
-	end
-
-	def get_checkpoint(checkpoint_id) #2160
-		response = self.class.get( "/checkpoints/#{checkpoint_id}", headers: {'authorization' => @auth_token} )
-		JSON.parse(response.body)
-	end
-
 end
